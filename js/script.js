8f0e7e308ac508969a7c01a026073ef8693ba627
admin-esn <admin@esn.paris>
2022-05-01 13:14:20 +0200

    premier commit

---
+++ js/script.js
@@ -0,0 +1,201 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+//     //Desactive le scroll pendant 7 seconde (soit modifier ici, soit mettre sur true et supprimer les lignes 5 a 9)
+//     firstAnimationIsFinish = true;
+//     setTimeout(firstAnimationFinish, 7000)
+
+//     function firstAnimationFinish(){
+//         globalThis.firstAnimationIsFinish = true
+//     }
+
+
+//     const COUNTER_THRESHOLD = 3; // Change this to decrease/increase senstivity
+//     const COUNTER_RESET_DURATION = 1000;
+  
+//     let animating = false;
+//     let counter = 0;
+
+//     var scrollDebounce = true;
+
+
+//     // Scroll between views
+//     function resetDelta() {
+//         counter = 0
+//     }
+
+//     let deboucncedReset = debounce(resetDelta, COUNTER_RESET_DURATION);
+
+//     function handleScroll(event) {
+//         //event.wheelDelta can be positive or negative based on the direction of scroll
+        
+        
+//         if (!firstAnimationIsFinish){
+//             console.log("normalement, tu ne peux pas scroller !");
+            
+//         }
+       
+//         counter += 1 * (Math.sign(event.wheelDelta));
+
+      
+
+//         //Scroll down if value of counter is negative and absolute value is greater than threshold
+       
+//         if (!animating && (Math.abs(counter) >= COUNTER_THRESHOLD) && counter < 0 && firstAnimationIsFinish && scrollDebounce) {
+           
+//             let targetSection = document.querySelector("[data-active]").nextElementSibling;
+       
+           
+//             if (targetSection) {
+//                 scrollToSection(targetSection);
+//             }
+
+           
+                
+//         }
+
+//          //Scroll up if value of counter is positive and absolute value is greater than threshold
+//             else if (!animating && (Math.abs(counter) >= COUNTER_THRESHOLD) && counter > 0 && firstAnimationIsFinish) {
+//                 let targetSection = document.querySelector("[data-active]").previousElementSibling;
+
+//                 if (targetSection) {
+//                     scrollToSection(targetSection);
+//                 }
+
+         
+//             }
+
+//             // prevent default scrolling behaviour of mouse wheel
+//             event.preventDefault()
+
+//             //Reset counter to 0 , 400 miliseconds after stopping the mousewheel
+//             deboucncedReset()
+       
+
+            
+
+            
+//     }
+
+//     function scrollToSection(target) {
+//         console.log("ça vient de scroller ma gueule"+target)
+//         animating = true;
+        
+        
+       
+//         target.scrollIntoView({
+//             behavior: 'smooth'
+//         });
+          
+//           delete document.querySelector("[data-active]").dataset.active;
+//           target.dataset.active = "";
+
+//           setTimeout(function(){
+//             animating = false;
+
+//           }, 300)
+
+          
+          
+       
+//       }
+
+//     function debounce(func, delay) {
+//         let debounceTimer
+//         return function() {
+//           const context = this
+//           const args = arguments
+//           clearTimeout(debounceTimer)
+//           debounceTimer
+//             = setTimeout(() => func.apply(context, args), delay)
+//         }
+//     }
+
+//      //Test support for passive listeners
+//   let supportsPassive = false;
+//   try {
+//     let options = Object.defineProperty({}, 'passive', {
+//       get: function() {
+//         supportsPassive = true;
+//       }
+//     });
+//     window.addEventListener("testPassive", null, options);
+//     window.removeEventListener("testPassive", null, options);
+//   } catch (e) {}
+
+//   let wheelOptions = supportsPassive ? {
+//     passive: false
+//   } : false;
+
+//   // Older browsers used 'mousewheel' event
+//   let wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+  
+//   document.addEventListener(wheelEvent, handleScroll, wheelOptions)
+
+  
+    
+
+    
+
+
+
+
+
+ 
+    // Bloc tu veux - Animation
+
+
+    // function([string1, string2],target id,[color1,color2])    
+    consoleText(['mais tu ne sais pas quelle formation choisir ?', 'mais tu n\'es pas sur que tu vas aimer ?', 'mais tu as peur de te lancer ?'], 'text',['pink','rebeccapurple','lightblue']);
+
+    function consoleText(words, id, colors) {
+    if (colors === undefined) colors = ['#fff'];
+    var visible = true;
+    var con = document.getElementById('console');
+    var letterCount = 1;
+    var x = 1;
+    var waiting = false;
+    var target = document.getElementById(id)
+    target.setAttribute('style', 'color:' + colors[0])
+    window.setInterval(function() {
+
+        if (letterCount === 0 && waiting === false) {
+        waiting = true;
+        target.innerHTML = words[0].substring(0, letterCount)
+        window.setTimeout(function() {
+            var usedColor = colors.shift();
+            colors.push(usedColor);
+            var usedWord = words.shift();
+            words.push(usedWord);
+            x = 1;
+            target.setAttribute('style', 'color:' + colors[0])
+            letterCount += x;
+            waiting = false;
+        }, 1000)
+        } else if (letterCount === words[0].length + 1 && waiting === false) {
+        waiting = true;
+        window.setTimeout(function() {
+            x = -1;
+            letterCount += x;
+            waiting = false;
+        }, 1000)
+        } else if (waiting === false) {
+        target.innerHTML = words[0].substring(0, letterCount)
+        letterCount += x;
+        }
+    }, 30)
+    window.setInterval(function() {
+        if (visible === true) {
+        con.className = 'console-underscore hidden'
+        visible = false;
+
+        } else {
+        con.className = 'console-underscore'
+
+        visible = true;
+        }
+    }, 400)
+    }
+
+
+});
