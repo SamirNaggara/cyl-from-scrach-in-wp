f012d3462f3e643039a4b6ce6f10236f3bb6c3b8
GorgeousGoat <gregory.drv@gmail.com>
2022-06-06 14:47:31 +0200

    essaie contact

--- contact.php
+++ contact.php
@@ -1,7 +1,11 @@
 <?php
-if (function_exists('mail')) {
-    echo 'mail() is available';
-} else {
-    echo 'mail() has been disabled';
-}
+  $dest = "zaackz@yahoo.fr";
+  $subjetc = "Test Email";
+  $body = "Hi this is a test email send by a php script";
+  $headers = "From: gregory.drv@gmail.com";
+  if (mail($dest, $subjetc, $body, $headers)) {
+    echo "Email successfully sent to $dest ...";
+  } else {
+    echo "Failed to send email...";
+  }
 ?>
